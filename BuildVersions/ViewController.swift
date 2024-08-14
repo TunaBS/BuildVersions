4a9aa8bbe10d0ec66ee10e8cf9fc57d4bc70870f
fairuz_ahmed_tuna <fairuz.ahmed@brainstation-23.com>
2024-08-14 17:06:20 +0600

    Initial Commit

---
+++ BuildVersions/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BuildVersions
+//
+//  Created by BS00880 on 14/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
